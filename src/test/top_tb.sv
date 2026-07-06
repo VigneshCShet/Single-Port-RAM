062dd7f0bf32314680814001e8d64f0089d0aaa2
VigneshCShet <vigneshshet1@gmail.com>
2026-07-06 19:51:56 +0530

    Refactor top_tb.sv by removing unused code
    Removed commented-out code and cleaned up the testbench for clarity.

--- src/test/top_tb.sv
+++ src/test/top_tb.sv
@@ -4,18 +4,8 @@ module tb;
 	logic reset;
 	
 	event trig, trig_2;
-	
-	//integer pass_log;
-	//integer fail_log;
-	
+
 	ram_if rif(clk, reset);
-	
-	/*ram rm(
-	  .clk(clk), 
-		.reset(reset), 
-		.data_in(rif.data_in), .address(rif.address), .write_en(rif.write_en), .read_en(rif.read_en), 
-		.data_out(rif.data_out)
-	);*/
 	
 	RAM rm(
           .clk(clk), // Clk input
@@ -67,41 +57,27 @@ module tb;
 	test t_ram     = new(rif.drv, rif.mon, rif.scr);
 	read_write_test t_r_w  = new(rif.drv, rif.mon, rif.scr);
 	write_read_test t_w_r = new(rif.drv, rif.mon, rif.scr);
-	//transaction_read tx_r;
 	
 	
 	initial begin
-	
-	  //pass_log = $fopen("pass_log.txt", "w");
-    //fail_log = $fopen("fail_log.txt", "w");
 		
-		//$fdisplay(pass_log, "Running Read Followed by write\n\n");
-		//$fdisplay(fail_log, "Running Read Followed by write\n\n");
 	  $display("\n \n \nRunning Read Followed by write \n \n");
 	  t_r_w.run();
 	  
 	  $display("\n \n \nRunning Write Followed by Read \n \n");
-	  //$fdisplay(pass_log, "Running Write Followed by Read\n\n");
-		//$fdisplay(fail_log, "Running Write Followed by Read\n\n");
+		
 	  fork
 	    t_w_r.run();
 	    ->trig;
 	  join
 	  
 	  $display("\n \n \nRunning Regressions\n \n");
-	  //$fdisplay(pass_log, "Running Regressions\n\n");
-		//$fdisplay(fail_log, "Running Regressions\n\n");
+
 	  fork
 	    t_ram.run();
 	    ->trig_2;
 	  join
 	  	  
-		
-		
-		
-		//tx_r = new();
-		//env.gen.tx = tx_r;
-		//t_w.env.run();
 		$finish;
 	end
 endmodule
